c6aa6c2281055def8249c006c89776de87d7ca7d
pcf26536 <muffwaindan@gmail.com>
2019-09-13 09:59:00 +0300

    [start #168473398] add faqs view section

---
+++ src/components/faqsView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FAQs = () => {
+  return(
+    <section className="faqs-view">
+      <div className="faqs-title">FAQs</div>
+
+    </section>
+  );
+};
+
+export default FAQs;

1c17389fc96624e82d5bf54e3c867d938727b72c
pcf26536 <muffwaindan@gmail.com>
2019-09-13 11:04:41 +0300

    [ft #168473398] update FAQ list function

--- src/components/utils/FAQsList.js
+++ src/components/utils/FAQsList.js
@@ -3,12 +3,14 @@ import formatBody from "./faqBody";
 import React from "react";
 
 const FAQsList = (list) => {
-
+    return list.map(item => (
+      GenerateFAQ(item)
+    ));
 };
 
 const GenerateFAQ = (faq) => {
   return (
-      <div>
+      <div key={faq.key}>
         {accordion.toggle(faq.quiz)}
         {accordion.panel(
             formatBody(faq.answer)

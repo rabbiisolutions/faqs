814c068f980b51631f2c02c2e0cb86f375fad65a
pcf26536 <muffwaindan@gmail.com>
2019-09-13 10:27:11 +0300

    [ft #168473398] add clients and tutors faqs objects

--- src/components/faqsView.js
+++ src/components/faqsView.js
@@ -4,7 +4,26 @@ const FAQs = () => {
   return(
     <section className="faqs-view">
       <div className="faqs-title">FAQs</div>
+      <div className="tabs-view">
+        <div className="tabs">
+          <div className="tab-link tutors" onClick="openPage('Home', this, 'red')">
+            Tutors
+          </div>
+          <div className="tab-link clients" onClick="openPage('News', this, 'green')">
+            Clients
+          </div>
+        </div>
 
+        <div className="tabs-content">
+          <div id="tutors" className="tab-content">
+
+          </div>
+
+          <div id="clients" className="tab-content open">
+
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/constants/tutorFAQs.js
@@ -0,0 +1,73 @@
+const tutorFAQs = {
+  name: 'tutor-faqs',
+  faqs: [
+    {
+      key: 21,
+      quiz: 'Who does home tuition?',
+      answer: ['Rabbii TS provides home tuition jobs across Nairobi, Kiambu, Machakos, Nakuru' +
+      ' and Mombasa counties. Our home tutors are categorized into two:', '⦁ Experts – These' +
+      ' are tutors with knowledge, skill and some little work experience but do not have' +
+      ' certification. They include campus students pursuing B.Ed. and have already gone for a ' +
+      'teaching practice, graduates from the schools of art and music, gifted sportsmen etc.',
+        '⦁ Professionals – these are experienced and certified personnel who offer home tuition' +
+        ' services. They include TSC Certified tutors, graduates from art and music schools, ' +
+        'masters students, professional chefs etc.', ]
+    },
+    {
+      key: 22,
+      quiz: 'How to become a home tutor?',
+      answer: ['Becoming a home tutor at Rabbii TS is simple; click the sign up tab above and' +
+      ' click register as a tutor. Depending on your qualifications for the homeschooling job,' +
+      ' you can sign up as an expert or professional. Ensure you provide accurate and honest ' +
+      'information about yourself as it will be verified first before you are assigned a client.']
+    },
+    {
+      key: 23,
+      quiz: 'How can I balance between being a home tutor and my normal schedule?',
+      answer: ['We value both our tutors and our clients, and our major objective is to give the' +
+      ' best services to our customers. Signing up as a tutor at Rabbii TS will mean that you are a' +
+      ' part of us and therefore will have to plan your time very well so that you don’t ' +
+      'inconvenience the client.', ]
+    },
+    {
+      key: 24,
+      quiz: 'As a home tutor, can I teach more than one subject?',
+      answer: ['Yes, you can provide homeschooling services in more than one area so long as you' +
+      ' have qualifications to teach in that area. However, you are only allowed a maximum of' +
+      ' two classes for every individual student you are assigned.', ]
+    },
+    {
+      key: 25,
+      quiz: 'How do I plan for a homeschooling class?',
+      answer: ['It depends on client’s expectation, Rabbii TS will always notify you on the days ' +
+      'of the week that the client would like to be taught.',]
+    },
+    {
+      key: 26,
+      quiz: 'Where do I get teaching resources like books and past papers?',
+      answer: ['You will be required to use your own resources for the tuition, but as time' +
+      ' advances, we shall provide all learning and teaching resources on this website. For' +
+      ' skill tuition, it will be a requirement that the client provides the learning resource' +
+      ' while the tutor will provide teaching resources.',]
+    },
+    {
+      key: 27,
+      quiz: 'How do I get a client who needs home tuition?',
+      answer: ['Rabbii TS will notify you as soon as a client best fit for your qualifications' +
+      ' requests for a tutor.', ]
+    },
+    {
+      key: 28,
+      quiz: 'How can I be paid for being a home tutor?',
+      answer: ['Expert tutors are paid KES.600 per hour while professional tutors are paid KES. 900' +
+      ' per hour.', ]
+    },
+    {
+      key: 29,
+      quiz: 'How long does a standard home tuition session last?',
+      answer: ['Our standard session lasts 1 hour 30 mins, but the client may choose to extend' +
+      ' the session depending on his/her preference.', ]
+    }
+  ]};
+
+export default tutorFAQs;

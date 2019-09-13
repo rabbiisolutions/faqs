ee93018b155b2f954bfde12286c1b4b76e9bd0ca
pcf26536 <muffwaindan@gmail.com>
2019-09-13 17:01:10 +0300

    [ft #168473398] add footer mixin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import FAQs from "./components/faqsView";
+import backDropHandler from "./eventHandlers/backDrop";
+import SideBar from "./components/mobile/sideBar";
+import MobileNavBar from "./components/mobile/navBar";
+import MobileFooter from "./components/mobile/footer";
+import SignUpModal from "./components/signUpModal";
 
 function App() {
   return (
       <Router>
+        <MobileNavBar/>
         <FAQs/>
+        <SideBar/>
+        <SignUpModal/>
+        <div className="page-mask hidden" onClick={e => backDropHandler(e)} />
+        <MobileFooter/>
       </Router>
   );
 }

a630bbab8f41a70f1a8e9390b4c27aa9e1ba4b2b
pcf26536 <muffwaindan@gmail.com>
2019-09-13 13:01:11 +0300

    [ft #168473398] add sidebar menu

---
+++ src/constants/texts.js
@@ -0,0 +1,11 @@
+const menuTitle = 'Menu';
+const menuItems = ['Home', 'Our Services', 'About Us', 'FAQs', 'Contact Us', 'Trending'];
+const ourServices = ['TeeCha'];
+const hidden = 'hidden';
+
+
+const texts = {
+  menuTitle: menuTitle, menuItems: menuItems, ourServices: ourServices, hidden: hidden
+};
+
+export default texts;

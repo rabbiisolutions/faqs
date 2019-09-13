0f52789f674687157af6618998f18e0d060e5386
pcf26536 <muffwaindan@gmail.com>
2019-09-13 12:40:46 +0300

    [ft #168473398] add event handlers for tab clicks

--- src/components/faqsView.js
+++ src/components/faqsView.js
@@ -2,6 +2,7 @@ import React from "react";
 import FAQsList from "./utils/FAQsList";
 import tutorFAQs from "../constants/tutorFAQs";
 import clientFAQs from "../constants/clientFAQs";
+import handleTabs from "../eventHandlers/tabs";
 
 const FAQs = () => {
   return(
@@ -9,20 +10,20 @@ const FAQs = () => {
       <div className="faqs-title">FAQs</div>
       <div className="tabs-view">
         <div className="tabs">
-          <div className="tab-link tutors">
+          <div className="tab-link tutors" onClick={e => handleTabs(e)}>
             Tutors
           </div>
-          <div className="tab-link clients">
+          <div className="tab-link clients active" onClick={e => handleTabs(e)}>
             Clients
           </div>
         </div>
 
         <div className="tabs-content">
-          <div id="tutors" className="tab-content">
+          <div id="tutors" className="tab-content hidden">
             {FAQsList(tutorFAQs.faqs)}
           </div>
 
-          <div id="clients" className="tab-content open">
+          <div id="clients" className="tab-content">
             {FAQsList(clientFAQs.faqs)}
           </div>
         </div>

a0b06845af2a943f0decc8f01aede729b322a28b
pcf26536 <muffwaindan@gmail.com>
2019-09-17 15:04:54 +0300

    [ft #168473398] update tabs color and layout

--- src/components/faqsView.js
+++ src/components/faqsView.js
@@ -16,20 +16,20 @@ const FAQs = () => {
       <div className="faqs-title">FAQs</div>
       <div className="tabs-view">
         <div className="tabs">
-          <div className="tab-link tutors" onClick={e => handleTabs(e)}>
+          <div className="tab-link tutors active" onClick={e => handleTabs(e)}>
             Tutors
           </div>
-          <div className="tab-link clients active" onClick={e => handleTabs(e)}>
+          <div className="tab-link clients" onClick={e => handleTabs(e)}>
             Clients
           </div>
         </div>
 
         <div className="tabs-content">
-          <div id="tutors" className="tab-content hidden">
+          <div id="tutors" className="tab-content">
             {FAQsList(tutorFAQs.faqs)}
           </div>
 
-          <div id="clients" className="tab-content">
+          <div id="clients" className="tab-content hidden">
             {FAQsList(clientFAQs.faqs)}
           </div>
         </div>
